7efd14856809d9518b113cc0fa5024436fa555d2
dung0811 <103187128+dung2511@users.noreply.github.com>
2023-08-04 22:01:26 +0700

    new

---
+++ page/theme/frontend/js/main.js
@@ -0,0 +1,8 @@
+var PROGRAM = (function () {
+    // viết các function ở đây
+  return {
+    _: function () {
+      // gọi lại các function ở đây
+    },
+  };
+})();
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["page/*.{html,js}" ],
+  theme: {
+    extend: {
+      screens: {
+        dp: "1920px"
+      }
+    },
+  },
+  plugins: [],
+}
+
