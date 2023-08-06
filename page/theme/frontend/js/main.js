13b02386ec5be67edc05ffb142b00834e65c31cc
dung0811 <103187128+dung2511@users.noreply.github.com>
2023-08-06 23:00:45 +0700

    update

--- page/theme/frontend/js/main.js
+++ page/theme/frontend/js/main.js
@@ -1,18 +1,22 @@
 var PROGRAM = (function () {
   var slideLandingSale = function () {
     const sl_landingSale = document.querySelector(".sl-landing_sale");
-    if(sl_landingSale.length === 0) return;
-    var swiperLangdingSale = new Swiper(sl_landingSale, {
-
-    })
+    if (sl_landingSale.length === 0) return;
+    var swiperLangdingSale = new Swiper(sl_landingSale, {});
   };
-  var slideImgLanding = function () {
-    const sl_imagesLanding = document.querySelector(".swiper-images-landing");
-    if(sl_imagesLanding.length === 0) return;
-    var swiperImagesLanding = new Swiper (sl_imagesLanding, {
-      slidesPerView: 1
-    })
-  }
+  var slideImgLanding = function (sliderId) {
+    const slider = new Swiper(`[data-slider-id="${sliderId}"]`, {
+      slidesPerView: 1,
+    });
+    document.addEventListener("DOMContentLoaded", function () {
+      const sliders = document.querySelectorAll(".swiper-images-landing");
+      if(sliders.length === 0) return;
+      sliders.forEach((slider) => {
+        const sliderId = slider.dataset.sliderId;
+        slideImgLanding(sliderId);
+      });
+    });
+  };
   return {
     _: function () {
       // gọi lại các function ở đây
