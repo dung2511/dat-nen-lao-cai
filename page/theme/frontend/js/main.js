a7364370339e347d5645552b348f965de33fab98
dung0811 <103187128+dung2511@users.noreply.github.com>
2023-08-06 19:19:18 +0700

    update

--- page/theme/frontend/js/main.js
+++ page/theme/frontend/js/main.js
@@ -3,12 +3,21 @@ var PROGRAM = (function () {
     const sl_landingSale = document.querySelector(".sl-landing_sale");
     if(sl_landingSale.length === 0) return;
     var swiperLangdingSale = new Swiper(sl_landingSale, {
-      
+
     })
   };
+  var slideImgLanding = function () {
+    const sl_imagesLanding = document.querySelector(".swiper-images-landing");
+    if(sl_imagesLanding.length === 0) return;
+    var swiperImagesLanding = new Swiper (sl_imagesLanding, {
+      slidesPerView: 1
+    })
+  }
   return {
     _: function () {
       // gọi lại các function ở đây
+      slideImgLanding();
     },
   };
 })();
+PROGRAM._();
