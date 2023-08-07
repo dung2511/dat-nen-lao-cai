5cfb5b07d4a737828146a746d6f026abecbba2fe
dung0811 <103187128+dung2511@users.noreply.github.com>
2023-08-07 09:21:11 +0700

    update

--- page/theme/frontend/js/main.js
+++ page/theme/frontend/js/main.js
@@ -1,8 +1,39 @@
 var PROGRAM = (function () {
-  var slideLandingSale = function () {
-    const sl_landingSale = document.querySelector(".sl-landing_sale");
+  var showMenu = function () {
+    var btnShowMenu = document.querySelector(".icon-menu");
+    var closeMenu = document.querySelector(".over-lay");
+    var menu = document.querySelector(".menu")
+    if(typeof btnShowMenu != "undefined") {
+      btnShowMenu.addEventListener("click",function() {
+        menu.classList.add("active");
+        closeMenu.classList.add("show")
+      })
+    }
+    if(typeof closeMenu != "undefined") {
+      closeMenu.addEventListener("click",function() {
+        menu.classList.remove("active");
+        closeMenu.classList.remove("show")
+      })
+    }
+  }
+  var slideLandingSaleHome = function () {
+    const sl_landingSale = document.querySelector(".sl-landing_mains");
     if (sl_landingSale.length === 0) return;
-    var swiperLangdingSale = new Swiper(sl_landingSale, {});
+    var swiperLangdingSale = new Swiper(sl_landingSale, {
+      slidesPerView: "auto",
+      spaceBetween: 16,
+      breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+        320: {
+          slidesPerView: 1,
+        },
+        425: {
+          slidesPerView: 2,
+        },
+      },
+    });
   };
   var slideImgLanding = function (sliderId) {
     const slider = new Swiper(`[data-slider-id="${sliderId}"]`, {
@@ -10,7 +41,7 @@ var PROGRAM = (function () {
     });
     document.addEventListener("DOMContentLoaded", function () {
       const sliders = document.querySelectorAll(".swiper-images-landing");
-      if(sliders.length === 0) return;
+      if (sliders.length === 0) return;
       sliders.forEach((slider) => {
         const sliderId = slider.dataset.sliderId;
         slideImgLanding(sliderId);
@@ -19,8 +50,9 @@ var PROGRAM = (function () {
   };
   return {
     _: function () {
-      // gọi lại các function ở đây
+      showMenu();
       slideImgLanding();
+      slideLandingSaleHome();
     },
   };
 })();
