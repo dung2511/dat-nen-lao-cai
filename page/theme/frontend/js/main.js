864028c698324d703a97b7901c462344dfd6d2bf
dung0811 <103187128+dung2511@users.noreply.github.com>
2023-08-22 10:55:56 +0700

    update

--- page/theme/frontend/js/main.js
+++ page/theme/frontend/js/main.js
@@ -1,4 +1,5 @@
 var PROGRAM = (function () {
+	new WOW().init();
 	var windows = window;
 	var showMenu = function () {
 		var btnShowMenu = document.querySelector(".icon-menu");
@@ -17,6 +18,7 @@ var PROGRAM = (function () {
 			});
 		}
 	};
+
 	var headerScroll = function () {
 		windows.addEventListener("scroll", function () {
 			var scrollPossition = windows.scrollY || windows.pageYOffset;
@@ -29,34 +31,116 @@ var PROGRAM = (function () {
 		});
 	};
 
+	return {
+		_: function () {
+			showMenu();
+			headerScroll();
+		},
+	};
+})();
+PROGRAM._();
+
+var SHOWFORM = (function () {
+	var showFormSearch = function () {
+		const openSearch = document.querySelectorAll(".btn-search");
+		const formSearch = document.querySelector(".form-search");
+		const closeSearch = document.querySelector(".box-close-search")
+		openSearch.forEach((itemOpenS) => {
+			if (formSearch != undefined) {
+				itemOpenS.addEventListener("click", function () {
+					if (window.getComputedStyle(formSearch).display === "none") {
+						formSearch.classList.add("active")
+					}
+				});
+				closeSearch.addEventListener("click", function () {
+					if (window.getComputedStyle(formSearch).display === "block") {
+						formSearch.classList.remove("active")
+					}
+				})
+			} else {
+				return;
+			}
+		})
+
+	}
+	var showModalRegister = function () {
+		const openModalRegister = document.querySelectorAll(".btn-register");
+		const closeModalRegister = document.querySelector(".box-close-modal");
+		const modalRegister = document.querySelector(".modal-register");
+		openModalRegister.forEach((itemOpen) => {
+			if (modalRegister != undefined) {
+				itemOpen.addEventListener("click", function () {
+					if (window.getComputedStyle(modalRegister).display === "none") {
+						modalRegister.classList.add("active");
+					}
+				});
+				closeModalRegister.addEventListener("click", function () {
+					if (window.getComputedStyle(modalRegister).display === "block") {
+						modalRegister.classList.remove("active");
+					}
+				});
+			} else {
+				return;
+			}
+		})
+
+	};
+	var select = function () {
+		if (typeof $ !== "undefined") {
+			if ($(".select-form").length == 0) return;
+			$(".select-form").each(function () {
+				$(this).select2();
+			});
+		}
+	}
+	return {
+		_: function () {
+			showFormSearch();
+			showModalRegister();
+			select();
+		},
+	};
+})();
+SHOWFORM._();
+
+var SLIDER = (function () {
 	var slideLandingSaleHome = function () {
 		const sl_landingSale = document.querySelector(".sl-landing_mains");
 		if (typeof sl_landingSale === "undefined") return;
 		var swiperLangdingSale = new Swiper(sl_landingSale, {
 			slidesPerView: "auto",
 			spaceBetween: 16,
+			navigation: {
+				nextEl: '.button-next-home',
+				prevEl: '.button-prev-home'
+			},
 			breakpoints: {
-				1366: {
-					slidesPerView: 4
-				},
+
 				1280: {
-					slidesPerView: 3.5
+					slidesPerView: 4,
+					loop:false,
 				},
 				1024: {
-					slidesPerView: 2.5,
+					slidesPerView: 3,
+					loop:true,
+
 				},
 				768: {
 					slidesPerView: 2,
+
 				},
 				320: {
 					slidesPerView: 1,
+
 				},
 				640: {
 					slidesPerView: 1.5,
+
 				},
 			},
 		});
 	};
+
 	var slideImgLanding = function () {
 		document.addEventListener("DOMContentLoaded", function () {
 			const sliders = document.querySelectorAll(".item-landing");
@@ -80,33 +164,11 @@ var PROGRAM = (function () {
 			});
 		});
 	};
-	var showModalRegister = function () {
-		const openModalRegister = document.querySelector(".btn-register");
-		const closeModalRegister = document.querySelector(".box-close-modal");
-		const modalRegister = document.querySelector(".modal-register");
-		if (modalRegister != undefined) {
-			openModalRegister.addEventListener("click", function () {
-				if (window.getComputedStyle(modalRegister).display === "none") {
-					modalRegister.classList.add("active");
-				}
-			});
-			closeModalRegister.addEventListener("click", function () {
-				if (window.getComputedStyle(modalRegister).display === "block") {
-					modalRegister.classList.remove("active");
-				}
-			});
-		} else {
-			return;
-		}
-	};
 	return {
 		_: function () {
-			showMenu();
 			slideImgLanding();
 			slideLandingSaleHome();
-			showModalRegister();
-			headerScroll();
 		},
 	};
 })();
-PROGRAM._();
+SLIDER._();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["page/*.{html,js}" ],
+  content: ["page/*.{html,js}"],
   theme: {
     extend: {
-      screens: {
-        dp: "1920px"
-      }
     },
   },
   plugins: [],
